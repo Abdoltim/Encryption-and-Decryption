4e7d2da89bb1fa360b72d0ea802c759b810fc7e6
Mohamed Radwan <mohamedhisham13@gmail.com>
2019-01-17 14:33:52 -0500

    Caesar Cipher

---
+++ Caesar Cipher.py
@@ -0,0 +1,24 @@
+
+# @Author: Mohamed Radwan
+# @Date: 2018-01-17
+# Rotates a list of characters based on the inputed rotate number
+# to add an easy way for encryption
+
+import string
+import collections
+
+
+def caesar(rotate_string, number_to_rotate_by):
+    upper = collections.deque(string.ascii_uppercase)
+    lower = collections.deque(string.ascii_lowercase)
+
+
+    upper.rotate(number_to_rotate_by)
+    lower.rotate(number_to_rotate_by)
+
+    upper = '' .join(list(upper))
+    lower = '' .join(list(lower))
+
+    return rotate_string.translate(string.maketrans(string.ascii_uppercase, upper)).translate(string.maketrans(string.ascii_lowercase, lower))
+
+
