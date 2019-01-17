e352470e47a1ebd891dd58e7db487b698980f309
Mohamed Radwan <mohamedhisham13@gmail.com>
2019-01-17 14:46:43 -0500

    Implemented the Caeser Chipher function that moves the characters in a string

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -1,4 +1,3 @@
-
 # @Author: Mohamed Radwan
 # @Date: 2018-01-17
 # Rotates a list of characters based on the inputed rotate number
@@ -12,13 +11,13 @@ def caesar(rotate_string, number_to_rotate_by):
     upper = collections.deque(string.ascii_uppercase)
     lower = collections.deque(string.ascii_lowercase)
 
-
     upper.rotate(number_to_rotate_by)
     lower.rotate(number_to_rotate_by)
 
-    upper = '' .join(list(upper))
-    lower = '' .join(list(lower))
+    upper = ''.join(list(upper))
+    lower = ''.join(list(lower))
 
-    return rotate_string.translate(string.maketrans(string.ascii_uppercase, upper)).translate(string.maketrans(string.ascii_lowercase, lower))
+    return rotate_string.translate(str.maketrans(string.ascii_uppercase, upper)).translate(
+        str.maketrans(string.ascii_lowercase, lower))
 
 
