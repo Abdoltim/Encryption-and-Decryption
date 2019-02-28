a8f6ea599bfc545f8ad4271c86a8bef8f0f26d58
Abdoltim <a.elnakla@hotmail.com>
2019-02-28 15:08:28 -0500

    Fixed Decryption

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -8,14 +8,14 @@ import string
 from tkinter import *
 
 
-def popupmsg(msg):
-    popup = tk.Tk()
-    popup.wm_title("!")
-    label = ttk.Label(popup, text=msg, font=NORM_FONT)
-    label.pack(side="top", fill="x", pady=10)
-    B1 = ttk.Button(popup, text="Okay", command=popup.destroy)
-    B1.pack()
-    popup.mainloop()
+# def popupmsg(msg):
+#     popup = tk.Tk()
+#     popup.wm_title("!")
+#     label = ttk.Label(popup, text=msg, font=NORM_FONT)
+#     label.pack(side="top", fill="x", pady=10)
+#     B1 = ttk.Button(popup, text="Okay", command=popup.destroy)
+#     B1.pack()
+#     popup.mainloop()
 
 
 def encryption(rotate_string, number_to_rotate_by):
@@ -34,13 +34,15 @@ def encryption(rotate_string, number_to_rotate_by):
 
 
 def encrypt():
-    encrypted = encryption(e1.get(), int(e2.get()))
+    key = int(e2.get())
+    encrypted = encryption(e1.get(), key)
     print("Your encryption is: " + encrypted)
 
 
 def decrypt():
-    decrypted = encryption(e1.get(), -int(e2.get()))
-    popupmsg("Your decryption is: " + decrypted)
+    key = int(e2.get())
+    decrypted = encryption(e1.get(),-key)
+    print("Your decryption is: " + decrypted)
 
 master = Tk()
 Label(master, text="Message").grid(row=0)
@@ -48,7 +50,6 @@ Label(master, text="Key").grid(row=1)
 
 e1 = Entry(master)
 e2 = Entry(master, show="*")
-
 e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
 
@@ -57,17 +58,3 @@ Button(master, text='Encrypt', command=encrypt).grid(row=3, column=0, sticky=W, 
 Button(master, text='Decrypt', command=decrypt).grid(row=4, column=0, sticky=W, pady=4)
 
 mainloop()
-
-# stop = True
-# print("Welcome to my encryption and decryption software")
-# while (stop):
-#
-#         service = input("1 for encryption\n2 for decryption\n")
-#         string = raw_input("Please input your text\n")
-#         key = input("Please input the key\n")
-#         if service == 1:
-#             print("The encrypted text is: ", caesar(string, key))
-#         elif service == 2:
-#             print("The decrypted text is: ", caesar(string, - key))
-#         else:
-#             print("Thats an invalid input please try again")
